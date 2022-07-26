8201abe4a3ec54c14c5c64847833f5615bdf3804
khanpranto <97869460+khanpranto@users.noreply.github.com>
2022-07-26 13:03:40 +0600

    Add files via upload

---
+++ beecrowd1008.py
@@ -0,0 +1,9 @@
+employess_number = int(input())
+per_hour_salary = int(input())
+working_hour = float(input())
+
+salary = working_hour*per_hour_salary
+
+print(F"NUMBER = %d" %employess_number)
+print(F"SALARY = U$ {salary:.2F}")
+
