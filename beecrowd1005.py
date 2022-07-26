30689e5d9852d4e9f1b44c90eca6fc9a681d05a2
khanpranto <97869460+khanpranto@users.noreply.github.com>
2022-07-26 12:39:07 +0600

    Add files via upload

---
+++ beecrowd1005.py
@@ -0,0 +1,5 @@
+a = float(input())
+b = float(input())
+
+avarage = (((a*3.5)+(b*7.5))/11)
+print(f"MEDIA = {avarage:.5f}")
---
+++ beecrowd1006.py
@@ -0,0 +1,6 @@
+a = float(input())
+b = float(input())
+c = float(input())
+
+avarage = (((a*2)+(b*3)+(c*5))/10)
+print(f"MEDIA = {avarage:.1f}")
